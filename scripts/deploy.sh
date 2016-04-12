d327a91b02eb98bbd26d0d4bf96c6740033ad447
nilar <nicolas.larche@rca.fr>
2016-04-12 17:18:53 +0200

    Add travis deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS" = "true" ]
+then
+  git config --global user.email "gh-pages@localhost"
+  git config --global user.name "npm gh-pages"
+fi
+
+./node_modules/.bin/gh-pages \
+  # ADJUST YOUR REMOTE HERE
+  --repo https://$GITHUB_TOKEN@github.com/nlarche/curriculum_vitae.git \
+  # TO HIDE YOUR $GITHUB_TOKEN!
+  # this is really important
+  --silent
